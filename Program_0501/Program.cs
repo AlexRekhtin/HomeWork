528412cef7f65daf8164f37192bf3fe4977a3085
AlexRekhtin <sasha777--95@mail.ru>
2022-12-01 11:21:01 +0700

    Добавил Program_0501

--- Program_0501/Program.cs
+++ Program_0501/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.WriteLine("Введите размер массива");
+Console.WriteLine("Размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
+Console.WriteLine("Вот ваш массив: ");
 PrintArray(numbers);
 int count = 0;
 
